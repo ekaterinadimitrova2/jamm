e35a65da55aa340d5461c845311734fc2115a71c
Ekaterina Dimitrova <ekaterina.dimitrova@datastax.com>
2023-05-28 19:30:44 -0400

    Update script

--- run-tests.sh
+++ run-tests.sh
@@ -5,5 +5,40 @@
 # 
 # Prerequisites
 [ "x$EXECUTION_ID" != "x" ] || { echo "Variable EXECUTION_ID must be defined";  exit 1; }
-mvn ${EXECUTION_ID}
 
+mvn ${EXECUTION_ID}
+mvn ${EXECUTION_ID} -DjvmArgs="-Xmx16G -XX:ObjectAlignmentInBytes=16"
+
+case "$EXECUTION_ID" in 
+  surefire:test@test-default|surefire:test@test-jdk11-64|surefire:test@test-jdk17-64)
+    mvn ${EXECUTION_ID} -DjvmArgs="-XX:+UseCompressedClassPointers -XX:+UseCompressedOops"
+    mvn ${EXECUTION_ID} -DjvmArgs="-XX:-UseCompressedClassPointers -XX:-UseCompressedOops"
+    mvn ${EXECUTION_ID} -DjvmArgs="-XX:+UseCompressedClassPointers -XX:-UseCompressedOops"
+    ;;
+  *)
+    # do nothing
+    ;;
+esac
+
+
+case "$EXECUTION_ID" in
+  surefire:test@test-jdk17-32|surefire:test@test-jdk17-64)
+    # mvn ${EXECUTION_ID} -DjvmArgs="-XX:-UseEmptySlotsInSupers" This test currently fails, though it is commented out. Please check the README for more info
+    mvn ${EXECUTION_ID} -DjvmArgs="-XX:+UseEmptySlotsInSupers"       
+    ;;
+  *)
+    # do nothing
+    ;;
+esac
+
+mvn test -DjvmArgs="-XX:+EnableContended -XX:+RestrictContended"
+mvn test -DjvmArgs="-XX:+EnableContended -XX:-RestrictContended"
+case "$EXECUTION_ID" in
+  surefire:test@test-default|surefire:test@test-jdk8-32|surefire:test@test-jdk11-64|surefire:test@test-jdk11-32)
+    mvn ${EXECUTION_ID} -DjvmArgs="-XX:-EnableContended -XX:+RestrictContended" 
+    mvn ${EXECUTION_ID} -DjvmArgs="-XX:+EnableContended -XX:+RestrictContended -XX:ContendedPaddingWidth=64"      
+    ;;
+  *)
+    # do nothing
+    ;;
+esac
